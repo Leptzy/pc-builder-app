cc7733f7800f8e56bef50ad271bfee131ae5a8fe
Jared <ja.midavaine@gmail.com>
2026-04-23 09:38:19 -0600

    Added the React side of the project

---
+++ mern/client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="w-full p-6">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default App
